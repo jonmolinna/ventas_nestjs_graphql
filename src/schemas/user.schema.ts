38308983eda17918a2316dfccf7d12298a6a50f0
jonmolinna <jmolina2624@gmail.com>
2024-10-04 07:30:27 -0500

    User Schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,6 +1,7 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import userRole from 'src/user/dto/role.interface';
 
 export type UserDocument = HydratedDocument<User>;
 
@@ -11,11 +12,27 @@ export class User {
   _id: string;
 
   @Field()
+  @Prop({ required: true })
+  name: string;
+
+  @Field()
+  @Prop({ required: true })
+  surname: string;
+
+  @Field()
   @Prop({ required: true, unique: true })
   username: string;
 
   @Prop({ required: true })
   password: string;
+
+  @Field()
+  @Prop({
+    type: String,
+    required: true,
+    enum: [userRole.admin, userRole.ventas],
+  })
+  role: string;
 
   @Field()
   @Prop({ type: Date, default: Date.now })
