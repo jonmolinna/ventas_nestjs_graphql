446bfbc46ee38ed93f18c5bc11a3ffe84d19502f
jonmolinna <jmolina2624@gmail.com>
2024-10-18 06:46:34 -0500

    Graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,18 +6,21 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       driver: ApolloDriver,
       playground: true,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
     }),
     MongooseModule.forRoot(
       'mongodb+srv://admin:2OkSLdVwZfA6YIYR@cluster0.mvear.mongodb.net/',
     ),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
